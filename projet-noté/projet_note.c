26f96f3e7897f286d1ed3c5cb9df4aea6e4afd29
zebi31 <kilian.djouber2@gmail.com>
2024-09-26 15:53:10 +0200

    Projet_note

---
+++ projet-noté/projet_note.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+int main()
+{
+
+srand(time(NULL));
+int random_number = rand() % 100 + 1;
+int numbercible;
+int p;
+
+while(p==0){
+	printf("Choisissez un nombre:");
+	scanf("%d", &numbercible);
+	if (numbercible<random_number){
+		printf("C'est plus!\n");
+		p=0;}
+	else if (numbercible>random_number){
+		printf("C'est moins!\n");
+		p=0;}
+	else {
+		printf("Vous avez trouvez! Félicitation!!!\n");
+		p=1;}}
+exit(0);
+}
